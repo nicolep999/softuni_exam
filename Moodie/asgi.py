23d7bb9b710b56b02ef1d1f34fa4b630a6145bc9
nicolep999 <nicolepavlova9@gmail.com>
2025-08-05 17:51:26 +0300

    fix manage.py and asgi.py to use production settings on Railway

--- Moodie/asgi.py
+++ Moodie/asgi.py
@@ -7,8 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
+import os
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Moodie.settings")
+# Use production settings if on Railway
+if os.environ.get('RAILWAY_ENVIRONMENT'):
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Moodie.settings_production")
+else:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Moodie.settings")
 
 application = get_asgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,12 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Moodie.settings")
+    # Use production settings if on Railway
+    if os.environ.get('RAILWAY_ENVIRONMENT'):
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Moodie.settings_production")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Moodie.settings")
+    
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
